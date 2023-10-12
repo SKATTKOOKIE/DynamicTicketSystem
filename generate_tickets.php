9869259422970c2f53754a0b34c7829f4fc89a23
Brandon <118194245+SKATTKOOKIE@users.noreply.github.com>
2023-10-12 19:03:18 +0100

    'Added comments to display what each part does, hopefully this help understand the code a bit better'

--- generate_tickets.php
+++ generate_tickets.php
@@ -7,32 +7,48 @@
 <body>
     <h2>Generated Tickets:</h2>
     <?php
-    if (isset($_POST['attendee_name'])) {
-        $attendee_names = $_POST['attendee_name'];
+        // Check if attendee names have been submitted via the POST method
+        if (isset($_POST['attendee_name'])) 
+        {
+            // Retrieve the submitted attendee names
+            $attendee_names = $_POST['attendee_name'];
 
-        foreach ($attendee_names as $key => $name) {
-            $unique_id = generateUniqueId(10);
-            echo '<div class="ticket">';
-            echo '<p>Ticket ID: ' . $unique_id . '</p>';
-            echo '<p>Attendee Name: ' . htmlspecialchars($name) . '</p>';
-            echo '</div>';
+            // Iterate through the submitted attendee names and generate unique IDs
+            foreach ($attendee_names as $key => $name) 
+            {
+                // Generate a unique ID with a specified length using the generateUniqueId function
+                $unique_id = generateUniqueId(10);
+
+                // Display a ticket with the unique ID and the sanitized attendee name
+                echo '<div class="ticket">';
+                echo '<p>Ticket ID: ' . $unique_id . '</p>';
+                echo '<p>Attendee Name: ' . htmlspecialchars($name) . '</p>';
+                echo '</div>';
+            }
+        } 
+        else 
+        {
+            // Display a message if no data has been submitted
+            echo "No data submitted.";
         }
-    } else {
-        echo "No data submitted.";
-    }
 
-    function generateUniqueId($length)
-    {
-        $characters = '0123456789';
-        $unique_id = '';
-        $max = strlen($characters) - 1;
+        // Define a function to generate a unique ID with the specified length
+        function generateUniqueId($length)
+        {
+            // Define the characters to be used in the unique ID
+            $characters = '0123456789';
+            $unique_id = '';
+            $max = strlen($characters) - 1;
 
-        for ($i = 0; $i < $length; $i++) {
-            $unique_id .= $characters[mt_rand(0, $max)];
+            // Generate a unique ID by selecting random characters
+            for ($i = 0; $i < $length; $i++) 
+            {
+                $unique_id .= $characters[mt_rand(0, $max)];
+            }
+
+            // Return the generated unique ID
+            return $unique_id;
         }
-
-        return $unique_id;
-    }
     ?>
 </body>
 </html>
--- ticket_form.php
+++ ticket_form.php
@@ -14,19 +14,29 @@
     </div>
 
     <div class="form-container">
+        <!-- This is a HTML form that submits data to "generate_tickets.php" using the POST method. -->
         <form action="generate_tickets.php" method="post">
+            <!-- A container for input fields that will be dynamically generated based on user input. -->
             <div id="name_inputs">
                 <?php
-                if (isset($_POST['generate'])) {
-                    $ticketCount = (int) $_POST['ticket_count'];
+                    // Check if the 'generate' button has been pressed (form submitted).
+                    if (isset($_POST['generate'])) 
+                    {
+                        // Retrieve the number of tickets to generate from the form input.
+                        $ticketCount = (int) $_POST['ticket_count'];
 
-                    for ($i = 1; $i <= $ticketCount; $i++) {
-                        echo '<input class="nameInput" type="text" name="attendee_name[]" placeholder="Attendee Name ' . $i . '" required>';
-                        echo '<br>';
+                        // Loop to generate input fields for attendee names based on the number of tickets.
+                        for ($i = 1; $i <= $ticketCount; $i++) 
+                        {
+                            // Create an input field for attendee name with a unique placeholder.
+                            echo '<input class="nameInput" type="text" name="attendee_name[]" placeholder="Attendee Name ' . $i . '" required>';
+                            // Insert a line break for better spacing between input fields.
+                            echo '<br>';
+                        }
                     }
-                }
                 ?>
             </div>
+            <!-- A submit button to trigger the form submission. -->
             <input type="submit" value="Generate Tickets">
         </form>
     </div>
